39fad9cb6c2536cc7ea43b36ff0ed8c8e27822f9
Sanghak Oeng <ingsanghak@gmail.com>
2023-08-07 12:49:22 -0400

    2nd henry commit

--- Server/app/config/db.config.js
+++ Server/app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url:"Henry:IRQfgJQB6osPyjOR@cluster0.ycyzmhk.mongodb.net/SportStore?retryWrites=true&w=majority"
+    url:"mongodb+srv://Henry:IRQfgJQB6osPyjOR@cluster0.ycyzmhk.mongodb.net/SportStore?retryWrites=true&w=majority
+    "
 };
